bf923d5ffc3b03e309d827855fb4276b0b2f28a9
H.D. 'Chip' McCullough IV <hdmccullough@gmail.com>
2020-01-28 14:47:54 -0500

    :gem:(project): Version 1.0.1   * Remove classes `KafkaActivity` and `KafkaFragment` for publishing

--- src/main/java/com/github/mcculloh213/kafkaesque/KafkaActivity.kt
+++ src/main/java/com/github/mcculloh213/kafkaesque/KafkaActivity.kt
@@ -1,16 +1,16 @@
-package com.github.mcculloh213.kafkaesque
-
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-
-open class KafkaActivity : AppCompatActivity(),
-    KafkaProducer by SimpleEventProducer(),
-    KafkaConsumer by CompoundStreamConsumer() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-    override fun onDestroy() {
-        unsubscribe()
-        super.onDestroy()
-    }
-}
+//package com.github.mcculloh213.kafkaesque
+//
+//import android.os.Bundle
+//import androidx.appcompat.app.AppCompatActivity
+//
+//open class KafkaActivity : AppCompatActivity(),
+//    KafkaProducer by SimpleEventProducer(),
+//    KafkaConsumer by CompoundStreamConsumer() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//    }
+//    override fun onDestroy() {
+//        unsubscribe()
+//        super.onDestroy()
+//    }
+//}
